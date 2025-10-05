b868b7b7586c5511bde1dad47cabce4f83a77efc
Fer162 <jenith.rodriguez@pi.edu.co>
2025-10-04 19:59:39 -0500

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:10000"
+workers = 1                  # 1 worker para ahorrar memoria
+threads = 2                  # Threads para concurrencia
+timeout = 120                # Timeout alto para Excel grandes
+loglevel = "info"
+accesslog = "-"
+errorlog = "-"
