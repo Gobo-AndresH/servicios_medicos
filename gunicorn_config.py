ba742ddd5160f6fdec7b00765d9a591fe650bf22
Fer162 <jenith.rodriguez@pi.edu.co>
2025-10-05 21:30:06 -0500

    Delete gunicorn_config.py

--- gunicorn_config.py
+++
@@ -1,7 +0,0 @@
-bind = "0.0.0.0:10000"
-workers = 1                  # 1 worker para ahorrar memoria
-threads = 2                  # Threads para concurrencia
-timeout = 120                # Timeout alto para Excel grandes
-loglevel = "info"
-accesslog = "-"
-errorlog = "-"
